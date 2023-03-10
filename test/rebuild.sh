3feae278eca85033de791ed3eaaa61e4673b61f4
Kevin Chevreuil - Kaisen <kaisen@kaisenlinux.org>
2023-03-10 21:36:22 +0100

    Import the new upstream release. Update the kaisen-drop-the-support-of-memtest86 quilt patch. Add the kaisen-drop-grub-signed-and-shim quilt patch. Remove the kaisen-add-the-support-of-efi-for-memtest86+ quilt patch.

--- test/rebuild.sh
+++ test/rebuild.sh
@@ -3,19 +3,27 @@
 # Rebuild an ISO image for a given timestamp
 #
 # Copyright 2021-2022 Holger Levsen <holger@layer-acht.org>
-# Copyright 2021-2022 Roland Clobus <rclobus@rclobus.nl>
+# Copyright 2021-2023 Roland Clobus <rclobus@rclobus.nl>
 # released under the GPLv2
 
 # Command line arguments:
 # 1) Image type
 # 2) Debian version
-# 3) [optional] Timestamp (format: YYYYMMDD'T'HHMMSS'Z')
+# 3) [optional] argument for the timestamp:
+#    - 'archive' (default): fetches the timestamp from the Debian archive
+#    - 'snapshot': fetches the latest timestamp from the snapshot server
+#    - A timestamp (format: YYYYMMDD'T'HHMMSS'Z'): a specific timestamp on the snapshot server
+# 4) [optional] argument for the origin of the d-i:
+#    - 'git' (default): rebuild the installer from git
+#    - 'archive': take the installer from the Debian archive
 
 # Environment variables:
 # http_proxy: The proxy that is used by live-build and wget
 # https_proxy: The proxy that is used by git
 # SNAPSHOT_TIMESTAMP: The timestamp to rebuild (format: YYYYMMDD'T'HHMMSS'Z')
 
+# This script can be run as root, but root rights are only required for a few commands.
+# You are advised to configure the user with 'visudo' instead.
 # Required entries in the sudoers file:
 #   Defaults env_keep += "SOURCE_DATE_EPOCH"
 #   Defaults env_keep += "LIVE_BUILD"
@@ -45,7 +53,8 @@ cleanup() {
 	cat <<EOF >summary.txt
 Configuration: ${CONFIGURATION}
 Debian version: ${DEBIAN_VERSION}
-Use latest snapshot: ${BUILD_LATEST}
+Use latest snapshot: ${BUILD_LATEST_DESC}
+Installer origin: ${INSTALLER_ORIGIN}
 Snapshot timestamp: ${SNAPSHOT_TIMESTAMP}
 Snapshot epoch: ${SOURCE_DATE_EPOCH}
 Live-build override: ${LIVE_BUILD_OVERRIDE}
@@ -54,61 +63,54 @@ Build result: ${BUILD_RESULT}
 Alternative timestamp: ${PROPOSED_SNAPSHOT_TIMESTAMP}
 Checksum: ${SHA256SUM}
 EOF
-}
-
-prepare_installer_cache() {
-	local SRC_FILENAME=$1
-	local DEST_FILENAME="$(pwd)/cache/installer_debian-installer/$(echo "${MIRROR}/dists/${DEBIAN_VERSION}/main/installer-amd64/current/images/${SRC_FILENAME}" | sed 's|/|_|g')"
-
-	mkdir -p $(dirname ${DEST_FILENAME})
-	cp -a ${DEBIAN_INSTALLER}/build/dest/${SRC_FILENAME} ${DEST_FILENAME}
+	touch summary.txt -d@${SOURCE_DATE_EPOCH}
 }
 
 parse_commandline_arguments() {
 	# Argument 1 = image type
 	case $1 in
 	"smallest-build")
-		export INSTALLER="none"
-		export PACKAGES=""
+		INSTALLER="none"
+		PACKAGES=""
 		;;
 	"cinnamon")
-		export INSTALLER="live"
-		export PACKAGES="live-task-cinnamon"
+		INSTALLER="live"
+		PACKAGES="live-task-cinnamon"
 		;;
 	"gnome")
-		export INSTALLER="live"
-		export PACKAGES="live-task-gnome"
+		INSTALLER="live"
+		PACKAGES="live-task-gnome"
 		;;
 	"kde")
-		export INSTALLER="live"
-		export PACKAGES="live-task-kde"
+		INSTALLER="live"
+		PACKAGES="live-task-kde"
 		;;
 	"lxde")
-		export INSTALLER="live"
-		export PACKAGES="live-task-lxde"
+		INSTALLER="live"
+		PACKAGES="live-task-lxde"
 		;;
 	"lxqt")
-		export INSTALLER="live"
-		export PACKAGES="live-task-lxqt"
+		INSTALLER="live"
+		PACKAGES="live-task-lxqt"
 		;;
 	"mate")
-		export INSTALLER="live"
-		export PACKAGES="live-task-mate"
+		INSTALLER="live"
+		PACKAGES="live-task-mate"
 		;;
 	"standard")
-		export INSTALLER="live"
-		export PACKAGES="live-task-standard"
+		INSTALLER="live"
+		PACKAGES="live-task-standard"
 		;;
 	"xfce")
-		export INSTALLER="live"
-		export PACKAGES="live-task-xfce"
+		INSTALLER="live"
+		PACKAGES="live-task-xfce"
 		;;
 	*)
 		output_echo "Error: Bad argument 1, image type: $1"
 		exit 1
 		;;
 	esac
-	export CONFIGURATION="$1"
+	CONFIGURATION="$1"
 
 	# Argument 2 = Debian version
 	# Use 'stable', 'testing' or 'unstable' or code names like 'sid'
@@ -116,14 +118,60 @@ parse_commandline_arguments() {
 		output_echo "Error: Bad argument 2, Debian version: it is empty"
 		exit 2
 	fi
-	export DEBIAN_VERSION="$2"
+	DEBIAN_VERSION="$2"
 
 	# Argument 3 = optional timestamp
-	export BUILD_LATEST=1
+	BUILD_LATEST="archive"
+	BUILD_LATEST_DESC="yes, from the main Debian archive"
 	if [ ! -z "$3" ]; then
-		export SNAPSHOT_TIMESTAMP=$3
-		BUILD_LATEST=0
+		case $3 in
+		"archive")
+			BUILD_LATEST="archive"
+			BUILD_LATEST_DESC="yes, from the main Debian archive"
+			;;
+		"snapshot")
+			BUILD_LATEST="snapshot"
+			BUILD_LATEST_DESC="yes, from the snapshot server"
+			;;
+		*)
+			SNAPSHOT_TIMESTAMP=$3
+			BUILD_LATEST="no"
+			BUILD_LATEST_DESC="no"
+			;;
+		esac
 	fi
+
+	INSTALLER_ORIGIN="git"
+	if [ ! -z "$4" ]; then
+		case $4 in
+		"git")
+			INSTALLER_ORIGIN="git"
+			;;
+		"archive")
+			INSTALLER_ORIGIN="${DEBIAN_VERSION}"
+			;;
+		*)
+			output_echo "Error: Bad argument 4, unknown value '$4' provided"
+			exit 4
+			;;
+		esac
+	fi
+}
+
+get_snapshot_from_archive() {
+	wget ${WGET_OPTIONS} http://deb.debian.org/debian/dists/${DEBIAN_VERSION}/InRelease --output-document latest
+	#
+	# Extract the timestamp from the InRelease file
+	#
+	# Input:
+	# ...
+	# Date: Sat, 23 Jul 2022 14:33:45 UTC
+	# ...
+	# Output:
+	# 20220723T143345Z
+	#
+	SNAPSHOT_TIMESTAMP=$(cat latest | awk '/^Date:/ { print substr($0, 7) }' | xargs -I this_date date --utc --date "this_date" +%Y%m%dT%H%M%SZ)
+	rm latest
 }
 
 #
@@ -136,36 +184,43 @@ trap cleanup INT TERM EXIT
 parse_commandline_arguments "$@"
 
 if $DEBUG; then
-	export WGET_OPTIONS=
-	export GIT_OPTIONS=
+	WGET_OPTIONS=
+	GIT_OPTIONS=
 else
-	export WGET_OPTIONS=--quiet
-	export GIT_OPTIONS=--quiet
+	WGET_OPTIONS=--quiet
+	GIT_OPTIONS=--quiet
 fi
 
 # No log required
 WGET_OPTIONS="${WGET_OPTIONS} --output-file /dev/null --timestamping"
 
 if [ ! -z "${LIVE_BUILD}" ]; then
-	export LIVE_BUILD_OVERRIDE=1
+	LIVE_BUILD_OVERRIDE=1
 else
-	export LIVE_BUILD_OVERRIDE=0
+	LIVE_BUILD_OVERRIDE=0
 	export LIVE_BUILD=${PWD}/live-build
 fi
-export DEBIAN_INSTALLER=${PWD}/debian-installer
+
+# Prepend sudo for the commands that require it (when not running as root)
+if [ "${EUID:-$(id -u)}" -ne 0 ]; then
+	SUDO=sudo
+fi
 
 # Use a fresh git clone
 if [ ! -d ${LIVE_BUILD} -a ${LIVE_BUILD_OVERRIDE} -eq 0 ]; then
 	git clone https://salsa.debian.org/live-team/live-build.git ${LIVE_BUILD} --single-branch --no-tags
 fi
-if [ ! -d ${DEBIAN_INSTALLER} -a "${INSTALLER}" == "live" ]; then
-	git clone https://salsa.debian.org/installer-team/debian-installer.git ${DEBIAN_INSTALLER} --single-branch --no-tags
-fi
 
-export LB_OUTPUT=lb_output.txt
+LB_OUTPUT=lb_output.txt
 rm -f ${LB_OUTPUT}
 
-if [ ${BUILD_LATEST} -eq 1 ]; then
+case ${BUILD_LATEST} in
+"archive")
+	# Use the timestamp of the current Debian archive
+	get_snapshot_from_archive
+	MIRROR=http://deb.debian.org/debian/
+	;;
+"snapshot")
 	# Use the timestamp of the latest mirror snapshot
 	wget ${WGET_OPTIONS} http://snapshot.notset.fr/mr/timestamp/debian/latest --output-document latest
 	#
@@ -180,12 +235,21 @@ if [ ${BUILD_LATEST} -eq 1 ]; then
 	# Output:
 	# 20210828T083909Z
 	#
-	export SNAPSHOT_TIMESTAMP=$(cat latest | awk '/"result":/ { split($0, a, "\""); print a[4] }')
+	SNAPSHOT_TIMESTAMP=$(cat latest | awk '/"result":/ { split($0, a, "\""); print a[4] }')
 	rm latest
-fi
+	MIRROR=http://snapshot.notset.fr/archive/debian/${SNAPSHOT_TIMESTAMP}
+	;;
+"no")
+	# The value of SNAPSHOT_TIMESTAMP was provided on the command line
+	MIRROR=http://snapshot.notset.fr/archive/debian/${SNAPSHOT_TIMESTAMP}
+	;;
+*)
+	echo "E: A new option to BUILD_LATEST has been added"
+	exit 1
+	;;
+esac
 # Convert SNAPSHOT_TIMESTAMP to Unix time (insert suitable formatting first)
 export SOURCE_DATE_EPOCH=$(date -d $(echo ${SNAPSHOT_TIMESTAMP} | awk '{ printf "%s-%s-%sT%s:%s:%sZ", substr($0,1,4), substr($0,5,2), substr($0,7,2), substr($0,10,2), substr($0,12,2), substr($0,14,2) }') +%s)
-export MIRROR=http://snapshot.notset.fr/archive/debian/${SNAPSHOT_TIMESTAMP}
 output_echo "Info: using the snapshot from ${SOURCE_DATE_EPOCH} (${SNAPSHOT_TIMESTAMP})"
 
 # Use the code from the actual timestamp
@@ -200,52 +264,28 @@ if [ ${LIVE_BUILD_OVERRIDE} -eq 0 ]; then
 else
 	output_echo "Info: using local live-build: $(lb --version)"
 fi
-if [ "${INSTALLER}" == "live" ]; then
-	pushd ${DEBIAN_INSTALLER} >/dev/null
-	git pull ${GIT_OPTIONS}
-	git checkout $(git rev-list -n 1 --min-age=${SOURCE_DATE_EPOCH} HEAD) ${GIT_OPTIONS}
-	git clean -Xdf ${GIT_OPTIONS}
-	# Specific patch to prevent /etc/apt/apt.conf.d of the host from influencing the dependency generator
-	sed -i -e 's|LANG=C apt-cache|LANG=C APT_CONFIG=./apt.udeb/apt.conf apt-cache|' build/util/pkg-list
-	output_echo "Info: using debian-installer from git version $(git log -n 1 --pretty=format:%H_%aI)"
-	popd >/dev/null
-fi
 
 # If the configuration folder already exists, re-create from scratch
 if [ -d config ]; then
-	sudo lb clean --purge
+	${SUDO} lb clean --purge
 	rm -fr config
 	rm -fr .build
 fi
 
-if [ "${INSTALLER}" == "live" ]; then
-	# Create a snapshot for the debian-installer
-	pushd ${DEBIAN_INSTALLER} >/dev/null
-	cd build
-	# MIRROR -> our snapshot URL, with disabled expiration
-	# TARGETS -> only these targets are required
-	MIRROR="[check-valid-until=no] ${MIRROR}" TARGETS="build_cdrom_gtk build_cdrom_isolinux" bash ./daily-build build-only
-	popd >/dev/null
-
-	# Copy the fresh installer to the cache. This will be used instead of the content of the online archive
-	prepare_installer_cache cdrom/vmlinuz
-	prepare_installer_cache cdrom/initrd.gz
-	prepare_installer_cache cdrom/gtk/vmlinuz
-	prepare_installer_cache cdrom/gtk/initrd.gz
-fi
-
 # Configuration for the live image:
 # - For /etc/apt/sources.list: Use the mirror from ${MIRROR}, no security, no updates
+# - The debian-installer is built from its git repository
 # - Don't cache the downloaded content
 # - To reduce some network traffic a proxy is implicitly used
 output_echo "Running lb config."
 lb config \
-	--parent-mirror-bootstrap ${MIRROR} \
-	--parent-mirror-binary ${MIRROR} \
+	--mirror-bootstrap ${MIRROR} \
+	--mirror-binary ${MIRROR} \
 	--security false \
 	--updates false \
 	--distribution ${DEBIAN_VERSION} \
 	--debian-installer ${INSTALLER} \
+	--debian-installer-distribution ${INSTALLER_ORIGIN} \
 	--cache-packages false \
 	2>&1 | tee $LB_OUTPUT
 
@@ -264,13 +304,13 @@ cp -a ${LIVE_BUILD}/examples/hooks/reproducible/* config/hooks/normal
 output_echo "Running lb build."
 
 set +e # We are interested in the result of 'lb build', so do not fail on errors
-sudo lb build | tee -a $LB_OUTPUT
-export BUILD_RESULT=$?
+${SUDO} lb build | tee -a $LB_OUTPUT
+BUILD_RESULT=$?
 set -e
 if [ ${BUILD_RESULT} -ne 0 ]; then
 	# Find the snapshot that matches 1 second before the current snapshot
 	wget ${WGET_OPTIONS} http://snapshot.notset.fr/mr/timestamp/debian/$(date --utc -d @$((${SOURCE_DATE_EPOCH} - 1)) +%Y%m%dT%H%M%SZ) --output-document but_latest
-	export PROPOSED_SNAPSHOT_TIMESTAMP=$(cat but_latest | awk '/"result":/ { split($0, a, "\""); print a[4] }')
+	PROPOSED_SNAPSHOT_TIMESTAMP=$(cat but_latest | awk '/"result":/ { split($0, a, "\""); print a[4] }')
 	rm but_latest
 
 	output_echo "Warning: lb build failed with ${BUILD_RESULT}. The latest snapshot might not be complete (yet). Try re-running the script with SNAPSHOT_TIMESTAMP=${PROPOSED_SNAPSHOT_TIMESTAMP}."
@@ -279,7 +319,17 @@ if [ ${BUILD_RESULT} -ne 0 ]; then
 fi
 
 # Calculate the checksum
-export SHA256SUM=$(sha256sum live-image-amd64.hybrid.iso | cut -f 1 -d " ")
+SHA256SUM=$(sha256sum live-image-amd64.hybrid.iso | cut -f 1 -d " ")
+
+if [ ${BUILD_LATEST} == "archive" ]; then
+	SNAPSHOT_TIMESTAMP_OLD=${SNAPSHOT_TIMESTAMP}
+	get_snapshot_from_archive
+	if [ ${SNAPSHOT_TIMESTAMP} != ${SNAPSHOT_TIMESTAMP_OLD} ]; then
+		output_echo "Warning: meanwhile the archive was updated. Try re-running the script."
+		PROPOSED_SNAPSHOT_TIMESTAMP="${BUILD_LATEST}"
+		exit 99
+	fi
+fi
 
 cleanup success
 # Turn off the trap
